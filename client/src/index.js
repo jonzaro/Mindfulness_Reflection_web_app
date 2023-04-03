36b51ec49441d3b4d8361b749ccd037924936275
Jon Zaro <writetojonzaro@gmail.com>
2023-04-03 11:48:11 -0700

    hashrouter attempt 2

--- client/src/index.js
+++ client/src/index.js
@@ -1,16 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import { BrowserRouter } from 'react-router-dom'
+import { HashRouter ,  BrowserRouter } from 'react-router-dom'
 import UserProvider from './context/UserProvider';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <HashRouter>
       <UserProvider>
         <App />
       </UserProvider>
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>
 );
