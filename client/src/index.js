ea015a32c64f731c3c5180e758a6eb37205c2b85
Jon Zaro <writetojonzaro@gmail.com>
2023-04-03 11:57:45 -0700

    back to browser router

--- client/src/index.js
+++ client/src/index.js
@@ -1,16 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import { HashRouter ,  BrowserRouter } from 'react-router-dom'
+import { BrowserRouter } from 'react-router-dom'
 import UserProvider from './context/UserProvider';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <HashRouter>
+    <BrowserRouter>
       <UserProvider>
         <App />
       </UserProvider>
-    </HashRouter>
+    </BrowserRouter>
   </React.StrictMode>
 );
