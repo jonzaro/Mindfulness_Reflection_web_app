8a328ed85292fc3a36cd376f9754b6b6d5b4b0e0
Jon Zaro <writetojonzaro@gmail.com>
2023-04-03 22:51:45 -0700

    connection string changes

--- server.js
+++ server.js
@@ -12,20 +12,7 @@ app.use(morgan('dev'))
 app.use(favicon(__dirname + '/public/favicon.ico'));
 
 
-mongoose.connect(process.env.MONGO_URI)
-    .then(() => {
-        console.log('Connected to database');
 
-        app.listen(port, (err) => {
-            if (err) {
-                throw new Error(err);
-            }
-            console.log('Server is Successfully Running, and App is listening on port ' + port);
-        });
-    })
-    .catch(err => {
-        console.error(err);
-    });
 
 
 
@@ -36,22 +23,21 @@ mongoose.connect(process.env.MONGO_URI)
 // )
 
 
-// const connectDB = async () => {
-//   try {
-//     const conn = await mongoose.connect(process.env.MONGO_URI);
-//     console.log(`MongoDB Connected: ${conn.connection.host}`);
-//   } catch (error) {
-//     console.log(error);
-//     process.exit(1);
-//   }
-// }
+const connectDB = async () => {
+  try {
+    const conn = await mongoose.connect("mongodb+srv://reflection:reflectionpass@cluster0.n9rb8od.mongodb.net/test");
+    console.log(`MongoDB Connected: ${conn.connection.host}`);
+  } catch (error) {
+    console.log(error);
+    process.exit(1);
+  }
+}
 
-// connectDB().then(() => {
-//   app.listen( () => {
-//       console.log("listening for requests");
-//   })
-// })
-
+connectDB().then(() => {
+  app.listen( () => {
+      console.log("listening for requests");
+  })
+})
 app.use('/auth', require('./routes/authRouter.js'))
 app.use('/api', expressjwt({ secret: process.env.SECRET, algorithms: ['HS256'] })) // req.user
 app.use('/api/reflection', require('./routes/reflectionRouter.js'))
@@ -68,6 +54,6 @@ app.all('*', (req,res) => {
 })
 
 
-// app.listen(9000, () => {
-//   console.log(`Server is running on local port 9000`)
-// })
+app.listen(9000, () => {
+  console.log(`Server is running on local port 9000`)
+})
